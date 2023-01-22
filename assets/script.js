44b3e56535f91a35f6053bd2d633184c01da5605
ynfanteerica <ynfanteerica@aol.com>
2023-01-22 00:21:21 -0500

    add more detail

---
+++ assets/script.js
@@ -0,0 +1,62 @@
+//Get data
+const nameInput = document.querySelector("#name");
+const email = document.querySelector("#email");
+const message = document.querySelector("#message");
+const success = document.querySelector("#success");
+const errorNodes = document.querySelectorAll(".error");
+
+//Validate data
+function validateForm(){
+
+    clearMessages();
+    let errorFlag = false;
+
+if(nameInput.value.length < 1) {
+  errorNodes[0].innerText = "Name cannot be blank";
+  nameInput.classList.add("error-border");
+  errorFlag = true;
+ }
+ if(!emailIsValid(email.value)) {
+    errorNodes[1].innerText = "Invalid email address";
+    email.classList.add("error-border");
+    errorFlag = true;
+ }
+ if(message.value.length < 1) {
+    errorNodes[2].innerText = "Please enter message";
+    message.classList.add("error-border");
+    errorFlag = true;
+ } 
+ if(!errorFlag) {
+    success.innerText = "Success!";
+ }
+}
+
+function clearMessages() {
+    for(let i = 0; i < errorNodes.length; i++) {
+        errorNodes[i].innerText = "";
+    }
+    success.innerText = "";
+    nameInput.classList.remove("error-border");
+    email.classList.remove("error-border");
+    message.classList.remove("error-border");
+}
+
+//Check if email is valid
+function emailIsValid(email) {
+    let pattern = /\S+@\S+\.\S+/;
+    return pattern.test(email);
+}
+
+function sendEmail(){
+    Email.send({
+        Host : "smtp.aol.com",
+        Username : "ynfanteerica@aol.com",
+        Password : "",
+        To : 'ynfanteerica@aol.com',
+        From : document.querySelector("#email").value,
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
