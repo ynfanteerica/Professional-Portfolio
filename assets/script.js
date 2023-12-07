36a095ea793ff2b7fb23e06ca0d005a8566e36d0
ynfanteerica <ynfanteerica@aol.com>
2023-12-07 18:33:46 -0500

    added a line on navbar

--- assets/script.js
+++ assets/script.js
@@ -71,3 +71,20 @@ contactform.addEventListener("submit", (e) => {
 
     }
   });
+function sendEmail(){
+  Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "username",
+    Password : "password",
+    To : 'them@website.com',
+    From : "you@isp.com",
+    Subject : "This is the subject",
+    Body : "And this is the body"
+}).then(
+  message => alert(message)
+);
+}
+
+
+
+
